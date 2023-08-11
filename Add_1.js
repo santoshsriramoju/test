b3349cd0057958b485f9df85a9879a6337ffd04c
santosh kumar sriramoju <santoshsriramoju2691@gmail.com>
2023-08-11 13:10:44 +0530

    Update Add_1.js

--- Add_1.js
+++ Add_1.js
@@ -1,9 +1,11 @@
 //Get sum from 1 to n(including n)
+//Part of udemy course
 function addUpTo(n) {
     let total = 0;
     for (let i = 1; i <= n; i++) {
         total += i;
     }
+    //summmation of the all the inputs
     return total;
 }
 
